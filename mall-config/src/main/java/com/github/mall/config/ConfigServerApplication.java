a08a78576e3a5ac53989657e9daa0c346f92453e
HPC <874694517@qq.com>
2019-04-20 19:53:13 +0800

    添加一些基础依赖,添加config,eureka

---
+++ mall-config/src/main/java/com/github/mall/config/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.github.mall.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 描述:
+ * 配置中心服务
+ *
+ * @author HPC
+ * @create 2019-04-20
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ mall-eureka/src/main/java/com/github/mall/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.github.mall.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 描述:
+ * 注册中心启动器
+ *
+ * @author HPC
+ * @create 2019-04-20
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
